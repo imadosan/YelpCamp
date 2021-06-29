6f5c5dc4f5585a603cf13902565252bec6afa16a
Imad Fayad <imad.fayad.if@gmail.com>
2021-06-29 21:27:28 +0200

    Added basic styling w/bootstrap

--- seeds/index.js
+++ seeds/index.js
@@ -22,9 +22,14 @@ const seedDB = async () => {
   await Campground.deleteMany({})
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000)
+    const randomPrice = Math.floor(Math.random() * 20) + 10
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: 'https://source.unsplash.com/collection/483251',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident alias, illum explicabo molestias, tempore harum animi temporibus odit asperiores dolore repudiandae dicta sapiente. Autem fugiat tempore exercitationem alias. Aliquam, laudantium?',
+      price: randomPrice,
     })
     await camp.save()
   }
