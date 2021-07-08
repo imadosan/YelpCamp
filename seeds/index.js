f7b77b0ecac371654c67ffcc7b6e4d5dea1348b1
imadosan <69857361+imadosan@users.noreply.github.com>
2021-07-08 23:13:42 +0200

    add authorization

--- seeds/index.js
+++ seeds/index.js
@@ -24,6 +24,7 @@ const seedDB = async () => {
     const random1000 = Math.floor(Math.random() * 1000)
     const randomPrice = Math.floor(Math.random() * 20) + 10
     const camp = new Campground({
+      author: '60e74d2893af523274619d6b',
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
       image: 'https://source.unsplash.com/collection/483251',
