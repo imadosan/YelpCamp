2c5f298205967959eb1ce988e9cd086eb8cbd658
imadosan <69857361+imadosan@users.noreply.github.com>
2021-07-11 20:46:34 +0200

    add geocoding on campground create

--- seeds/index.js
+++ seeds/index.js
@@ -1,7 +1,14 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const mongoose = require('mongoose')
 const cities = require('./cities')
 const { places, descriptors } = require('./seedHelpers')
 const Campground = require('../models/campground')
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding')
+const mapBoxToken = process.env.MAPBOX_TOKEN
+const geocoder = mbxGeocoding({ accessToken: mapBoxToken })
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
   useNewUrlParser: true,
@@ -22,22 +29,33 @@ const seedDB = async () => {
   await Campground.deleteMany({})
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000)
-    const randomPrice = Math.floor(Math.random() * 20) + 10
+    const name = `${sample(descriptors)} ${sample(places)}`
+    const price = Math.floor(Math.random() * 20) + 10
+    const location = `${cities[random1000].city}, ${cities[random1000].state}`
+    const geoData = await geocoder
+      .forwardGeocode({
+        query: location,
+        limit: 1,
+      })
+      .send()
+
     const camp = new Campground({
+      // YOUR USER ID
       author: '60e74d2893af523274619d6b',
-      location: `${cities[random1000].city}, ${cities[random1000].state}`,
-      title: `${sample(descriptors)} ${sample(places)}`,
+      title: name,
+      location: location,
       description:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident alias, illum explicabo molestias, tempore harum animi temporibus odit asperiores dolore repudiandae dicta sapiente. Autem fugiat tempore exercitationem alias. Aliquam, laudantium?',
-      price: randomPrice,
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias magni distinctio porro, cum dicta perspiciatis nam? Ad maiores, nisi voluptatum obcaecati magni neque omnis ab et nobis amet molestias exercitationem.',
+      price,
+      geometry: geoData.body.features[0].geometry,
       images: [
         {
           url: 'https://res.cloudinary.com/dtwwscejc/image/upload/v1625935609/YelpCamp/wwepaivmf6ik4iwmogs7.jpg',
-          filename: 'wwepaivmf6ik4iwmogs7',
+          filename: 'YelpCamp/hills_dflhtz',
         },
         {
           url: 'https://res.cloudinary.com/dtwwscejc/image/upload/v1625935609/YelpCamp/ceby5dsz2xlwskooyr7x.jpg',
-          filename: 'ceby5dsz2xlwskooyr7x',
+          filename: 'YelpCamp/hills_dflhtz',
         },
       ],
     })
